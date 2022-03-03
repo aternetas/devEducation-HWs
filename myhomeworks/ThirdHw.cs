c95477473c3c6279a969bf06656225d6286c6d43
Lena Falina <aternetas@gmail.com>
2022-03-03 18:16:44 +0300

    hw3 (task 3 & 4)

--- myhomeworks/ThirdHw.cs
+++ myhomeworks/ThirdHw.cs
@@ -7,6 +7,38 @@ using System.Threading.Tasks;
 namespace myhomeworks
 {
     static class ThirdHw
-    {
+    {   
+        // Пользователь вводит 1 число (A). Найдите количество положительных целых чисел, квадрат которых меньше A.
+        public static int GetCountOfPositiveNumbers(int a)
+        {
+            int countOfNumbers = 0;
+
+            while(countOfNumbers * countOfNumbers < a)
+            {
+                countOfNumbers++;
+            }
+            
+            return countOfNumbers - 1;
+        }
+
+        // Пользователь вводит 1 число (A). Вывести наибольший делитель (кроме самого A) числа A.
+        public static int GetBiggestDivisor(int a)
+        {
+            if (a < 0)
+            {
+                a *= -1;
+            }
+
+            int biggestDivisor = 0;
+
+            for (int i = a - 1; i != 1; i--)
+            {
+                if (a % i == 0 && a / i == 2)
+                {
+                    biggestDivisor = i;
+                }
+            }
+            return biggestDivisor;
+        }
     }
 }
